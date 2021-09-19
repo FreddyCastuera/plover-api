56b4477577bbe70bb6b688a7fa928c900c9795c7
Hector Hernandez Alcaraz <healcaraz@gmail.com>
2021-09-19 15:07:52 -0500

    Creating POST, GET and GET by id in payments

--- src/routers/payments.js
+++ src/routers/payments.js
@@ -1,8 +1,58 @@
 const express = require('express');
-const Payments = require('../usecases/payments');
-const router = express.Router()
+const router = express.Router();
 
-// prueba head
+// .: Importing usescases
+const Payments = require('../usecases/payments');
+
+// .: Methods
+
+// .: GET all payments
+router.get('/', async (request, response)=> {
+    try {
+        const allPayments = await Payments.getAllPayments(); 
+        response.status(200)
+        response.json({
+            success:true,
+            message:"Getting all payments",
+            data:{
+                payment: allPayments
+            }
+        })
+    }
+    catch(error) {
+        response.status(400)
+        response.json({
+            success: false,
+            error: error.message,
+            message: "Recovering payments went wrong try again..."
+        })
+    }
+});
+
+// .: GET payment by ID
+router.get('/:id', async (request, response)=> {
+    try{
+        const {id} = request.params;
+        const paymentIdFound = await Payments.getPaymentById(id)
+        response.json({
+            success: true,
+            messaje: `Payment with ${id} found`,
+            data:{
+                payment: paymentIdFound
+            }
+        })
+    }
+    catch(error) {
+        response.status(400)
+        response.json({
+            success: false,
+            error: error.message,
+            message: "id provided does not exist, try again with a valid id..."
+        })
+    }
+});
+
+// .: POST create a payment
 router.post('/', async (request, response)=> {
     console.log(request.body)
     try{
@@ -21,7 +71,8 @@ router.post('/', async (request, response)=> {
         response.status(400)
         response.json({
             success: false,
-            message: "Something went wronw... try again"
+            error: error.message,
+            message: "Creating new payment went wronw... try again"
         })
     }
 });
--- src/usecases/payments.js
+++ src/usecases/payments.js
@@ -1,11 +1,21 @@
 const Payments = require('../models/payments')
 
+// .: search all the payments in the DB
+async function getAllPayments(){
+    return Payments.find()
+}
+// .: search a payment by id
+async function getPaymentById(id){
+    return Payments.findById(id)
+}
+// .: create a new payment
 async function createPayment(payment){
     const {total, date, receipt} = payment
-
     return Payments.create({...payment})
 }
 
 module.exports = {
+    getAllPayments: getAllPayments,
     createPayment: createPayment,
+    getPaymentById: getPaymentById,
 }
