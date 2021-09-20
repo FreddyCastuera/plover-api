fd0132082b529c675e3bf498dab8700704ac3674
Hector Hernandez Alcaraz <healcaraz@gmail.com>
2021-09-19 20:43:32 -0500

    Creating Dentist model

--- src/models/dentist.js
+++ src/models/dentist.js
@@ -2,7 +2,7 @@ const mongoose = requite('mongoose');
 
 // .: Model Dentist ->
 
-const modelSchema = new mongoose.Schema({
+const dentistSchema = new mongoose.Schema({
     name: {
         type: String,
         minLength: 2,
@@ -34,6 +34,74 @@ const modelSchema = new mongoose.Schema({
         type: Number,
         minLength: 10,
         required: true
-    }
-    
-})
+    },
+    clinicName: {
+        type: String,
+        minLength: 2,
+        maxLength: 50,
+        required: true,
+        trim: true
+    },
+    clinicNumber: {
+        type: Number,
+        minLength: 10,
+        required: true
+    },
+    clinicAdress: {
+        type: String,
+        minLength: 2,
+        maxLength: 100,
+        required: true,
+        trim: true
+      },
+      neighborhood: {
+        type: String,
+        minLength: 2,
+        maxLength: 100,
+        required: true,
+        trim: true
+      },
+      zipCode: {
+        type: Number,
+        minLength: 5,
+        required: true
+      },
+      clinicEmail: {
+        type: String,
+        required: true,
+        match: /.*@.*\..*/,
+      },
+      career: {
+        type: String,
+        minLength: 8,
+        maxLength: 50,
+        required: true,
+        trim: true
+      },
+      college: {
+        type: String,
+        minLength: 8,
+        maxLength: 50,
+        required: true,
+        trim: true
+      },
+      profesionalLicense: {
+        type: String,
+        minLength: 7,
+        maxLength: 9,
+        required: true,
+        trim: true
+      },
+      password: {
+        type: String,
+        required: true
+      },
+      patients: {
+        type:[{ type: mongoose.Schema.Types.ObjectId, ref: 'posts' }],
+        default:[]
+      }
+
+},{timestamps: true})
+
+const model = mongoose.model('dentist', dentistSchema)
+module.exports = model
